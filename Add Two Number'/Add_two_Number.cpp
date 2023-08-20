1adb0332a14238003619ceec220eead6fee6bf56
Shubham Kumar Jha <shubhamkumarjha855@gmail.com>
2023-08-20 20:33:42 +0530

    I am learnig git And this is my first commit

---
+++ Add Two Number'/Add_two_Number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int firInteger, secondInteger;
+    cin>>firInteger>>secondInteger;
+    
+    cout <<(firInteger + secondInteger);
+
+    return 0;
+}
